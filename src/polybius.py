b9d16add4967d0db31157c7aedb38cc58a3bdcfd
Nicholas Zhang <nicholasz2510@gmail.com>
2020-10-10 01:31:26 -0600

    Update polybius.py

--- src/polybius.py
+++ src/polybius.py
@@ -1,12 +1,16 @@
 import discord
 
-f = open("secret.txt", "r")
+f = open('../secret.txt', 'r')
+
 
 class MyClient(discord.Client):
     async def on_ready(self):
         print('Logged on as {0}!'.format(self.user))
 
     async def on_message(self, message):
+        if message.content.startswith('$'):
+            print(str(message.author) + ': ' + message.content[1:])
+
         if message.author == client.user:
             return
 
@@ -14,7 +18,7 @@ class MyClient(discord.Client):
             await message.channel.send('TODO')
 
         elif message.content.startswith('$'):
-            await message.channel.send('Hello! I\'m a Discord bot <@449579826278563860> made!\nMy prefix is `-`\nFor '
+            await message.channel.send('Hello! I\'m a Discord bot <@449579826278563860> made!\nMy prefix is `$`\nFor '
                                        'a list of commands, type `$help` or `$info`')
 
 
